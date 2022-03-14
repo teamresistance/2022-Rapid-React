b0396e7f608594bb5d6fd18fda2d66db7520184c
JimHofm52 <JimHofmann52@gmail.com>
2022-03-13 22:33:43 -0400

    Branch for post Orlando

--- 2022-RR-Competition/src/main/java/frc/robot/Robot.java
+++ 2022-RR-Competition/src/main/java/frc/robot/Robot.java
@@ -2,6 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+!!! --- Preserve the code in Main.  DO NOT CHANGE MAIN ---!!!
+This branch is for code changes after the Orlando Regional.
+*/
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
