41f2d99ed1b04145ceb43925c62c90c4f88372f7
JimHofm52 <JimHofmann52@gmail.com>
2022-03-20 23:22:15 -0400

    CLimbTest, Pin/Slider Status test.

--- 2022-RR-Competition/src/main/java/frc/robot/testing/ClimbTest.java
+++ 2022-RR-Competition/src/main/java/frc/robot/testing/ClimbTest.java
@@ -25,11 +25,55 @@ public class ClimbTest {
         sdbUpdate();
     }
 
-    private static boolean sliderExt_FB(){return IO.sliderExt_L_FB.get() || IO.sliderExt_R_FB.get();}
-    
-    private static boolean lockPinAExt_FB(){return IO.lockPinAExt_L_FB.get() && IO.lockPinAExt_R_FB.get();}  //Or of pin A left & right
-    private static boolean lockPinBExt_FB(){return IO.lockPinBExt_L_FB.get() && IO.lockPinBExt_R_FB.get();}  //Or of pin A left & right
     // private static boolean sliderExt_FB(){return IO.sliderExt_L_FB.get() || IO.sliderExt_R_FB.get();}
+    
+    // private static boolean lockPinAExt_FB(){return IO.lockPinAExt_L_FB.get() && IO.lockPinAExt_R_FB.get();}  //Or of pin A left & right
+    // private static boolean lockPinBExt_FB(){return IO.lockPinBExt_L_FB.get() && IO.lockPinBExt_R_FB.get();}  //Or of pin A left & right
+    // private static boolean sliderExt_FB(){return IO.sliderExt_L_FB.get() || IO.sliderExt_R_FB.get();}
+
+    /**
+     * RET - both retracted
+     * <p>LEFT - Left extended, Right retracted
+     * <p>RIGHT - Left retracted, Right extended
+     * <p>EXT - both extended
+     */
+    private static enum eStsFB {RET, LEFT, RIGHT, EXT};
+
+    /**
+     * 
+     * @param in1 - Left extended feedback
+     * @param in2 - Right extended feedback
+     * @return status of combined Left & Right extended feedbacks.
+     * <p>RET - both retracted
+     * <p>LEFT - Left extended, Right retracted
+     * <p>RIGHT - Left retracted, Right extended
+     * <p>EXT - both extended
+     */
+    private static eStsFB e2DIStatus(boolean in1, boolean in2){
+        int tmp = in1 ? 1 : 0;
+        tmp += in2 ? 2 : 0;
+        switch(tmp){
+            case 0: return eStsFB.RET;
+            case 3: return eStsFB.EXT;
+            case 1: return eStsFB.LEFT;
+            default: return eStsFB.RIGHT;
+        }
+    }
+
+    /**Combined status of left & right locking pins A. */
+    private static eStsFB lockPinA_Sts(){
+        return e2DIStatus(IO.lockPinAExt_L_FB.get(), IO.lockPinAExt_R_FB.get());
+    }
+
+    /**Combined status of left & right locking pins B. */
+    private static eStsFB lockPinB_Sts(){
+        return e2DIStatus(IO.lockPinBExt_L_FB.get(), IO.lockPinBExt_R_FB.get());
+    }
+
+    /**Combined status of left & right slider feedbacks. */
+    private static eStsFB slider_Sts(){
+        return e2DIStatus(IO.sliderExt_L_FB.get(), IO.sliderExt_R_FB.get());
+    }
 
     private static void sdbUpdate(){
         SmartDashboard.putBoolean("Test/Climb/AExt_L_FB", IO.lockPinAExt_L_FB.get());
@@ -38,9 +82,9 @@ public class ClimbTest {
         SmartDashboard.putBoolean("Test/Climb/BRet_R_FB", IO.lockPinBExt_R_FB.get());
         SmartDashboard.putBoolean("Test/Climb/SExt_L_FB", IO.sliderExt_L_FB.get());
         SmartDashboard.putBoolean("Test/Climb/SExt_R_FB", IO.sliderExt_R_FB.get());
-        SmartDashboard.putBoolean("Test/Climb/SExt_FB L && B", !sliderExt_FB());
-        SmartDashboard.putBoolean("Test/Climb/AExt_FB L && B", lockPinAExt_FB());
-        SmartDashboard.putBoolean("Test/Climb/BExt_FB L && B", lockPinBExt_FB());
+        SmartDashboard.putString("Test/Climb/lockPinA Sts, L && B", lockPinA_Sts().toString());
+        SmartDashboard.putString("Test/Climb/lockPinB Sts, L && B", lockPinB_Sts().toString());
+        SmartDashboard.putString("Test/Climb/SExt Sts, L && B", slider_Sts().toString());
 
         SmartDashboard.putNumber("Test/Climb/Motor6_cmd", IO.climbMotor.get());
         SmartDashboard.putNumber("Test/Climb/Motor6_axis", -JS_IO.axCoDrvY.get());
