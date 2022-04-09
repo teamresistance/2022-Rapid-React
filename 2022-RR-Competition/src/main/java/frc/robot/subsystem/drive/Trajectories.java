11d21814f00853717640c0569de1fa4fa31c77c3
JimHofm52 <JimHofmann52@gmail.com>
2022-04-09 10:41:49 -0400

    Added to Trajectories

--- 2022-RR-Competition/src/main/java/frc/robot/subsystem/drive/Trajectories.java
+++ 2022-RR-Competition/src/main/java/frc/robot/subsystem/drive/Trajectories.java
@@ -11,7 +11,8 @@ public class Trajectories {
     private static double dfltPwr = 0.4;
     private static SendableChooser<String> chsr = new SendableChooser<String>();
     private static String[] chsrDesc = {
-        "getEmpty", "getCargo1", "getCargo2", "getCargo3", "getCargo4", "getCargo5", "getCargo6", "SnorfShootTest", "wayPtTest", "oneCargo", "twoBallAuto", "threeBallAuto"
+        "getEmpty", "ball3_A_B", "ball2_C","getCargo2", "getCargo3", "getCargo4", "getCargo5", 
+        "getCargo6", "SnorfShootTest", "wayPtTest", "oneCargo", "twoBallAuto", "threeBallAuto"
     };
 
     /**Initialize Traj chooser */
@@ -40,8 +41,12 @@ public class Trajectories {
             return getEmpty(pwr);
             case "SnorfShootTest":
             return snorfShootTest(pwr);
-            case "getCargo1":
-            return getCargo1(pwr);            
+            case "ball3_A_B":   //Start at P2, shoot, backup & swing around to A then B, return to P2 & shoot 2.
+            return ball3_A_B(pwr);
+            case "ball2_C":     //Start at P7, get C, return to P5 and Shoot 2
+            return ball2_C(pwr);
+            case "ball1_X":     //Start at P?, Shoot 1, backup
+            return ball1_X(pwr);
             case "getCargo2":
             return getCargo2(pwr);
             case "getCargo3":
@@ -136,29 +141,76 @@ public class Trajectories {
         return traj;
     }
 
-    public static ATrajFunction[] getCargo1(double pwr) { //3 Ball Auto
+    /**
+     * 3 ball auto.  Starting at P2 position against right fender and go for balls A & B.
+     * @param pwr
+     * @return
+     */
+    public static ATrajFunction[] ball3_A_B(double pwr) { //was getCargo1
         pwr = 1.0;
         ATrajFunction traj[] = {
-            new CoorOffset(-66.0, 4.0, -1.5),
-            // new ShootDrvAuto(false),
-            new ShootDrvAutoR(),
-            new TurnNMove(-66.0, -4.0, pwr), // already at -66 degrees, go back 4 feet
-            new TurnNMove(-66.0, 0.5, pwr),
-            new TankTurnHdg(90, 0.6, -0.6), //Turns
-            new SnorfDrvAuto(true), //Drop snorfler
-            new TurnNMove(90.0, 2.0, 0.5), //Pick up first ball
-            new TurnNMove(90.0, -1.5, 0.5), //Backs out
-            new SnorfDrvAuto(false), //Stops snorfler
-            new TankTurnHdg(182.0, 1.0, -1.0), //Turn towards next ball
-            new SnorfDrvAuto(true), //Start snorfle
-            new TurnNMove(182.0, 7.0, pwr), //Move forward
-            new TurnNMove(182.0, -0.2, pwr), //Brake
-            new SnorfDrvAuto(false), //Stop snorfle
-            new TurnNMove(182.0, -6.0, pwr), //Move Back
-            new TurnNMove(182.0, 0.1, pwr), //Brake
-            new TankTurnHdg(-62.0, 1.0, -1.0), //Turns, with adjustment for 
-            new TurnNMove(-62.0, 2.5, pwr),
-            new ShootDrvAuto(false),
+            new CoorOffset(-66.0, 4.0, -1.5),   //Adj offsets for P2 position
+            new ShootDrvAuto(null, false),      //Shoot right ball high
+            new TurnNMove(-66.0, -4.0, pwr),    //already at -66 degrees, go back 4 feet
+            new TurnNMove(-66.0, 0.5, 0.5),     //brake
+            new TankTurnHdg(90, 0.8, -0.2),     //Pivot right to 90 deg hdg, A ball
+            new SnorfDrvAuto(true),             //Drop snorfler
+            new TurnNMove(90.0, 2.0, 0.5),      //Fwd 2' along 90 deg.  Pick up A ball
+            new TurnNMove(90.0, -1.5, 0.5),     //Back out
+            new SnorfDrvAuto(false),            //Stops snorfler
+            new TankTurnHdg(190.0, 1.0, -0.3),  //Pivot towards B ball at 195 deg hdg
+            new SnorfDrvAuto(true),             //Start snorfle
+            // new TurnNMove(190.0, 8.0, pwr),     //Move forward to B Ball
+            new Waypt(7.0, -10.0, 0.8, 4),      //Move forward to B Ball
+            new TurnNMove(190.0, -0.2, 0.3),    //Brake
+            new SnorfDrvAuto(false),            //Stop snorfle
+            new TurnNMove(190.0, -7.0, pwr),    //Move Back
+            new TurnNMove(190.0, 0.2, 0.4),     //Brake
+            new TankTurnHdg(-62.0, 1.0, -1.0),  //Turns, with adjustment for Fender (starting point)
+            new TurnNMove(-66.0, 2.5, 0.5),     //Move to Fender
+            new ShootDrvAuto(false),            //Shoot both
+        };
+        return traj;
+    }
+
+    /**
+     * 2 ball auto.  Start at P7, get C, return to P5 and Shoot 2.
+     * @param pwr
+     * @return
+     */
+    public static ATrajFunction[] ball2_C(double pwr) {
+        pwr = 1.0;
+        ATrajFunction traj[] = {
+            new CoorOffset(-165.0, -5.0, -5.0), //Adj offsets for P7 position
+            new SnorfDrvAuto(true),             //Drop snorfler
+            new TrajDelay(0.5),                 //Wait 0.5 sec for Snorfe to settle.
+            new TurnNMove(-165.0, 7.0, pwr),    //already at -165 degrees, go fwd 7 feet
+            new TurnNMove(-165.0, -0.5, -0.5),  //brake
+            new SnorfDrvAuto(false),            //Stops snorfler
+            new TankTurnHdg(24, 0.8, -0.2),     //Pivot right to 24 deg hdg, towards P5
+            new TurnNMove(24.0, 10.0, pwr),     //Fwd 10' back to P5
+            new TrajDelay(0.5),                 //Wait 0.5 sec for bot to settle.
+            new ShootDrvAuto(false),            //Shoot both
+        };
+        return traj;
+    }
+
+    /**
+     * 1 ball auto.  Start at P1-6, Shoot right, backup passed line.
+     * @param pwr
+     * @return
+     */
+    public static ATrajFunction[] ball1_X(double pwr) {
+        pwr = 1.0;
+        ATrajFunction traj[] = {
+            // new CoorOffset(-66.0, 4.5, -0.5),   //Adj offsets for P1 position
+            new CoorOffset(-66.0, 4.0, -1.5),   //Adj offsets for P2 position
+            // new CoorOffset(-66.0, 3.5, -2.0),   //Adj offsets for P3 position
+            // new CoorOffset(24.0, -2.8, -3.4),   //Adj offsets for P4 position
+            // new CoorOffset(24.0, -1.8, -3.8),   //Adj offsets for P5 position
+            // new CoorOffset(24.0, -0.8, -4.2),   //Adj offsets for P6 position
+            new ShootDrvAuto(null, false),      //Shoot right
+            new TankTimed(1.5, -pwr, -pwr)      //Backup ~10'
         };
         return traj;
     }
@@ -311,273 +363,5 @@ public class Trajectories {
         };
         return traj;
     }
-
-
-    /*
-
-    public static ATrajFunction[] blue1(double pwr) {
-        ATrajFunction[] traj = {
-            new CoorOffset(-66, -5, -5),
-            new Waypt(-20, -20, pwr),
-
-        };
-        return traj;
-    }*/
-
-    // public static ATrajFunction[] getBounce(double pwr) {
-    //     ATrajFunction[] traj = {
-    //         new TurnNMove(0,   3.0, pwr), //2.8
-    //         new TurnNMove(270, 3.4, pwr), //3.2
-    //         new TurnNMove(270,-2.5, pwr),
-    //         new TurnNMove(230,-5.5, pwr), //6.3
-    //         new TurnNMove(315, 1.8, pwr), 
-    //         new TurnNMove(270, 7.3, pwr), //6.8
-    //         new TurnNMove(270,-6.2, pwr), //6.8
-    //         new TurnNMove(0,   5.7, pwr), //6
-    //         new TurnNMove(270, 8.2, pwr),
-    //         new TurnNMove(270,-1.8, pwr), //2.4
-    //         new TurnNMove(-10, 4.0, pwr)
-    //      };
-    //     return traj;
-    // }
-
-    // /**Runs a figure 8 pattern using various trajectory functions. First turn to the right.
-    //  * Uses MoveOnHdg for straight runs.
-    //  * <p>Est. for various radii
-    //  * <p> 3.5' right 0.72.0, 0.0
-    //  */
-    // public static ATrajFunction[] getFigure8R(double pwr) {
-    //     ATrajFunction traj[] = {
-    //     new MoveOnHdg(0.0, 5.0),
-    //     new CirToHdgTank(3.5, 5.5, 3.5, 135.0, -0.71, 0.0 ), //Turn right half circle
-    //     // new CirToHdgTank(3.5, 0.0, 3.5, 270.0, 0.8, -0.2 ), //Turn right half circle
-    //     // new CirToHdgTank(3.5, 0.0, 3.5, 45.0, 0.8, -0.2 ), //Turn right half circle
-    //     // new MoveOnHdg(45, 6.4, 1.0),
-
-    //     // new CirToHdgTank(-3.5, 0.0, 3.5, -135.0, -0.2, 0.8 ), //Turn left half circle
-    //     };
-    //     return traj;
-    // }
-
-    // /**Runs a figure 8 pattern using various trajectory functions. First turn to the left.
-    //  * Uses MoveOnHdg for straight runs.
-    //  */
-    // public static ATrajFunction[] getFigure8L(double pwr) {
-    //     // System.out.println("---------- Made it here: Traj Sq " + pwr + " ----------------");
-    //     ATrajFunction traj[] = {
-    //     new TankTurnHdg(180, 0.1, 0.85), //Turn left half circle
-    //     new TankTurnHdg(-30, 0.1, 0.85),  //continue circle to 30, more then 360.
-    //     new MoveOnHdg(-30.0, 11.0, 1.0),
-    //     new TankTurnHdg(-180, 0.85, 0.1), //Turn right half circle
-    //     new TankTurnHdg(-150, 0.85, 0.1),  //continue circle to 30, more then 360.
-    //     new MoveOnHdg(-150, 11.0, 1.0),
-    //     new TankTurnHdg(10, 0.1, 0.85), //Turn left half circle
-    //     };
-    //     return traj;
-    // }
-
-    // /**Runs a figure 8 pattern using various trajectory functions. First turn to the right.
-    //  * Uses Waypoints for straight runs.
-    //  */
-    // public static ATrajFunction[] getFigure8WPT(double pwr) {
-    //     pwr = 0.8;
-    //     ATrajFunction traj[] = {
-    //         new Waypt( 0.0, 9.0, 0.6),
-    //         new Waypt( 9.0, 9.0, 1.0, 10),
-    //         new Waypt( 9.0, 0.0, 1.0, 10),
-    //         new Waypt( 0.0, 0.0, 1.0, 10),
-    //         new TankTurnHdg(5, -0.8, 0.8)
-    //     };
-    //     return traj;
-    // }
-
-    // /**Runs a square pattern using TurnNMove trajectory function. */
-    // public static ATrajFunction[] getSquare_TNM(double pwr) {
-    //     // System.out.println("---------- Made it here: Traj Sq " + pwr + " ----------------");
-    //     ATrajFunction traj[] = {
-    //     new TurnNMove(0,   6, pwr),
-    //     new TurnNMove(90,  6, pwr),
-    //     new TurnNMove(180, 6, pwr),
-    //     new TurnNMove(270, 6, pwr),
-    //     new TurnNMove(360, 0, pwr)
-    //     };
-    //     return traj;
-    // }
-
-    // /**Runs a square pattern using MoveOnHdg trajectory function. */
-    // public static ATrajFunction[] getSquare_MOH(double pwr) {
-    //     // System.out.println("---------- Made it here: Traj Sq " + pwr + " ----------------");
-    //     ATrajFunction traj[] = {
-    //         new MoveOnHdg(   0, 6, 1.0, 30),
-    //         new MoveOnHdg(  90, 6, 1.0, 30),
-    //         new MoveOnHdg( 180, 6, 1.0, 30),
-    //         new MoveOnHdg( 270, 6, 1.0, 30),
-    //         new MoveOnHdg( 350, 0, 1.0, 30)
-    //         // new MoveOnHdg(  90, 6, 1.0, 30),
-    //         // new MoveOnHdg(   0, 6, 1.0, 30),
-    //         // new MoveOnHdg( -90, 6, 1.0, 30),
-    //         // new MoveOnHdg(-180, 6, 1.0, 30),
-    //         // new MoveOnHdg(-350, 0, 1.0, 30)
-    //     };
-    //     return traj;
-    // }
-
-    // //-------------- Galaxtic Search ------------------------
-    // //------------- Path in name open Snorfler --------------
-    // public static ATrajFunction[] getPathRedA(double pwr) {
-    //     ATrajFunction[] traj = {
-    //         new SnorfDrvAuto(true),
-    //         new TurnNMove(0,   3.0, pwr),
-    //         new TurnNMove(25,  5.3, pwr),
-    //         new TurnNMove(-85, 6.2, pwr),
-    //         new TurnNMove(0,  10.3, pwr),
-    //         new SnorfDrvAuto(false)
-    //      };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getPathBluA(double pwr) {
-    //     ATrajFunction[] traj = { 
-    //         new SnorfDrvAuto(true),
-    //         new TurnNMove(22,  9.3, pwr),
-    //         new TurnNMove(-70, 6.5, pwr),
-    //         new TurnNMove(27,  4.0, pwr),
-    //         new TurnNMove(10,  4.0, pwr),
-    //         new SnorfDrvAuto(false)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getPathRedB(double pwr) {
-    //     ATrajFunction[] traj = {
-    //         new SnorfDrvAuto(true),
-    //         new TurnNMove(-33, 3.5, 65),
-    //         new TurnNMove(40,  7.0, pwr),
-    //         new TurnNMove(-60, 7.1, pwr),
-    //         new TurnNMove(-10, 6.5, pwr),
-    //         new SnorfDrvAuto(false)
-    //      };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getPathBluB(double pwr) {
-    //     ATrajFunction[] traj = {
-    //         new SnorfDrvAuto(true),
-    //         new TurnNMove(11,  1.0, pwr),
-    //         new TurnNMove(-60, 5.0, pwr),
-    //         new TurnNMove(40,  4.0, pwr),
-    //         new TurnNMove(20,  4.0, pwr),
-    //         new SnorfDrvAuto(false)
-    //      };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getPathBlue(double pwr) {
-    //     ATrajFunction[] traj = {
-    //         new SnorfDrvAuto(true),
-    //         new TurnNMove(21,  9.5, pwr),  //Move to BlueA ball1
-    //         new TurnNMove(-72, 8.7, pwr),  //Move to B6 thru BlueB ball1
-    //         new TurnNMove(48, 10.0, pwr),   //Move to BlueAB ball3
-    //         new SnorfDrvAuto(false)
-    //      };
-    //     return traj;
-    // }
-
-    // /**
-    //  * Establishes the Trajectory array from the Raspberry Pi
-    //  * @param pwr - applied default power to turns and runs
-    //  * @return the Trajectoy array for the path assigned by the Raspberry Pi
-    //  */
-    // public static ATrajFunction[] getPathGalaxtic(double pwr) {
-    //     switch (RPI.galacticShooter()) {
-    //         case 1:
-    //             return getPathRedA(pwr);
-    //         case 2:
-    //         return getPathBlue(pwr);   //inside Blue
-    //         // return getPathBluB(70);   //inside Blue
-    //         case 3:
-    //             return getPathRedB(pwr);
-    //         case 4:
-    //             return getPathBlue(pwr);   //outside Blue
-    //             // return getPathBluA(70);   //outside Blue
-    //         default:
-    //             System.out.println("Bad Galaxtic path - " + RPI.galacticShooter());
-    //             return getEmpty(0);
-    //     }
-    // }
-
-    // public static ATrajFunction[] getCurveTry(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new MoveOnHdg(0.0, 3.5, 1.0),
-    //         new CirToHdgCurve(2.5, 3.5, 2.5, 135, 1.0, 0.5),
-    //         // new CurveTurn(1.0, 0.9, 1.0),
-    //         // new TurnNMove(0.0, 1.0, 4.0),
-    //         // new CurveTurn(1.0, 1.0, 1.0),
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve1_1(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(1.0, 1.0, 3.0)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve1_7(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(1.0, 0.75, 3.0)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve1_5(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(1.0, 0.5, 3.0)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve7_1(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(0.75, 1.0, 3.0)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve7_7(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(0.75, 0.75, 3.0)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve7_5(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(0.75, 0.5, 3.0)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve5_1(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(0.5, 1.0, 3.0)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve5_7(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(0.5, 0.75, 3.0)
-    //     };
-    //     return traj;
-    // }
-
-    // public static ATrajFunction[] getCurve5_5(double fwd) {
-    //     ATrajFunction traj[] = {
-    //         new CurveTurnTm(0.5, 0.5, 3.0)
-    //     };
-    //     return traj;
-    // }
 
 }
